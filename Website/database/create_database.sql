2bb6baddad22241cd012ece941d91a63ba78603c
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2015-05-07 17:41:51 +0200

    Added scripts for basic database creation

---
+++ Website/database/create_database.sql
@@ -0,0 +1,1 @@
+CREATE SCHEMA IF NOT EXISTS `raspi_surveillance` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
---
+++ Website/database/migrations/2015_05_07_145243_create_cameras_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCamerasTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('cameras', function($table)
+        {
+            $table->increments('id');
+            $table->string('ip_address', 45);
+            $table->string('name', 32)->nullable();
+            
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('cameras');
+	}
+
+}
