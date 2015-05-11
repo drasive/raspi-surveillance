d7e4851c7368c5045ae6b6ab0eb72ed702ce7846
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2015-05-11 21:40:19 +0200

    Made fonts on error pages easier to read in IE

--- Website/resources/views/errors/404.blade.php
+++ Website/resources/views/errors/404.blade.php
@@ -1,48 +1,48 @@
-﻿<html lang="en">
+<html lang="en">
 <head>
-	<meta charset="utf-8">
+    <meta charset="utf-8">
     <title>Page Not Found - Raspi Surveillance</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-	<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
 
-	<style>
-		body {
-			margin: 0;
-			padding: 0;
-			width: 100%;
-			height: 100%;
-			color: #B0BEC5;
-			display: table;
-			font-weight: 100;
-			font-family: 'Lato';
-		}
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            color: #B0BEC5;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
 
-		.container {
-			text-align: center;
-			display: table-cell;
-			vertical-align: middle;
-		}
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
 
-		.content {
-			text-align: center;
-			display: inline-block;
-		}
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
 
-		.title {
-			font-size: 72px;
-			margin-bottom: 40px;
-		}
-	</style>
+        .title {
+            font-size: 72px;
+            margin-bottom: 40px;
+        }
+    </style>
 </head>
 <body>
-	<div class="container">
-		<div class="content">
-			<div class="title">Page Not Found</div>
-			
-			<h2>Sorry, the requested page does not exist.</h2>
-			<h2>Go <a href="javascript:history.back()">back</a> or to the <a href="./">homepage</a>.</h2>
-		</div>
-	</div>
+    <div class="container">
+        <div class="content">
+            <div class="title">Page Not Found</div>
+            
+            <h2>Sorry, the requested page does not exist.</h2>
+            <h2>Go <a href="javascript:history.back()">back</a> or to the <a href="./">homepage</a>.</h2>
+        </div>
+    </div>
 </body>
 </html>
--- Website/resources/views/errors/503.blade.php
+++ Website/resources/views/errors/503.blade.php
@@ -4,44 +4,44 @@
     <title>Server Error - Raspi Surveillance</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-	<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
 
-	<style>
-		body {
-			margin: 0;
-			padding: 0;
-			width: 100%;
-			height: 100%;
-			color: #B0BEC5;
-			display: table;
-			font-weight: 100;
-			font-family: 'Lato';
-		}
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            color: #B0BEC5;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
 
-		.container {
-			text-align: center;
-			display: table-cell;
-			vertical-align: middle;
-		}
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
 
-		.content {
-			text-align: center;
-			display: inline-block;
-		}
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
 
-		.title {
-			font-size: 72px;
-			margin-bottom: 40px;
-		}
-	</style>
+        .title {
+            font-size: 72px;
+            margin-bottom: 40px;
+        }
+    </style>
 </head>
 <body>
-	<div class="container">
-		<div class="content">
-			<div class="title">Down for Maintenance.</div>
-			
-			<h2>Please come back in a few minutes.</h2>
-		</div>
-	</div>
+    <div class="container">
+        <div class="content">
+            <div class="title">Down for Maintenance.</div>
+            
+            <h2>Please come back in a few minutes.</h2>
+        </div>
+    </div>
 </body>
 </html>
