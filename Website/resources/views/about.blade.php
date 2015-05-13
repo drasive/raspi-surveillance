94d046f9885fc69ac35ae91a3055f9d24f8a6a10
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2015-05-13 20:34:29 +0200

    Filled in the content on the about page

--- Website/resources/views/about.blade.php
+++ Website/resources/views/about.blade.php
@@ -12,24 +12,29 @@
 
 <div class="row">
     <div class="col-md-6">
-        <h3>uf/What</h3>
+        <h3>What</h3>
         <p>
-            A simple website for viewing and managing the upcomming events of a cultural institution.
+            A website running on a Raspberry Pi for viewing and managing livestreams from Raspberry Pi cameras in the local network.<br />
+            The Raspberri Pi that the website is running on can be set into three modes: Off, videostreaming and motion detection.
+            In motion detection mode, a short video will be recorded every time the camera captures a moving object in its field of view.
+            The recorded videos can be viewed and managed directly through the website.<br />
+            <br />
+            <i>This is a practice project and is in no way intended or suitable to be used for serious surceillance use cases.</i>
         </p>
     </div>
     <div class="col-md-6">
-        <h3>uf/Technique</h3>
+        <h3>Technique</h3>
         <p>
-            Frontend: A HTML5 & CSS3 website using Bootstrap and jQuery<br />
-            Backend: PHP 5.4 using Laravel 4.2 running on IIS 8, MySQL 5.7
+            Frontend: A HTML5 & CSS3 website using AngularJS and Bootstrap<br />
+            Backend: PHP 5.6 using Laravel 5.0 running on Apache 2.4, MySQL Server 5.6
         </p>
     </div>
 </div>
 <div class="row">
     <div class="col-md-6">
-        <h3>uf/Why</h3>
+        <h3>Why</h3>
         <p>
-            The creation of a website for a cultural institution was a school project for module #151 at the <a href="http://home.gibm.ch/">GIBM</a> (Switzerland).
+            The creation of this website was a school project for software engineering module #152 at the <a href="http://home.gibm.ch/">GIBM</a> (Switzerland).
         </p>
     </div>
     <div class="col-md-6">
@@ -38,12 +43,12 @@
         <h4>Developer</h4>
         <p>
             Dimitri Vranken<br />
-            Mail: <a href="mailto:dimitri.vranken@hotmail.ch">dimitri.vranken@hotmail.ch</a><br />
+            Mail: <a href="mailto:dimitri.vranken@hotmail.ch?subject=Raspi%20Surveillance">dimitri.vranken@hotmail.ch</a><br />
         </p>
         <h4>Expert</h4>
         <p>
             B&eacute;atrice Duc<br />
-            Mail: <a href="mailto:b.duc@gibmit.ch">b.duc@gibmit.ch</a><br />
+            Mail: <a href="mailto:b.duc@gibmit.ch?subject=Raspi%20Surveillance">b.duc@gibmit.ch</a><br />
         </p>
     </div>
 </div>
