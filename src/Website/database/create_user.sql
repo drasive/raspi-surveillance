c50da9eafb7462109e9f5b2148edc4d7b4081b3d
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2015-06-04 10:00:09 +0200

    User script to create MySQL user

---
+++ src/Website/database/create_user.sql
@@ -0,0 +1,6 @@
+CREATE USER 'raspi_surveillance'@'localhost';
+
+GRANT INSERT ON raspi_surveillance.* TO 'raspi_surveillance'@'localhost';
+GRANT SELECT ON raspi_surveillance.* TO 'raspi_surveillance'@'localhost';
+GRANT UPDATE ON raspi_surveillance.* TO 'raspi_surveillance'@'localhost';
+GRANT DELETE ON raspi_surveillance.* TO 'raspi_surveillance'@'localhost';
